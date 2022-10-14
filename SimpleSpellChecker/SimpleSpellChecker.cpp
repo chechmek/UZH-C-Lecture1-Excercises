fdf36e5bbcfa02a814b9f6484d526cdf9c9f51f2
Roman Babukh <romanklut89@gmail.com>
2022-10-14 19:05:11 +0200

    Begined with 3d exercise

---
+++ SimpleSpellChecker/SimpleSpellChecker.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    ifstream dictionaryFile(argv[1]);
+    ifstream textFile(argv[2]);
+    
+    set<string> dictionary;
+    dictionary.
+}
+
---
+++ UnixToMSDOS/UnixToMSDOS.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+void convert_to_unix(ofstream& ofs, ifstream& ifs);
+void convert_to_msdos(ofstream& ofs, ifstream& ifs);
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello World!\n";
+
+	ifstream ifs(argv[1]);
+	ofstream ofs(argv[2]);
+	string instruction(argv[3]);
+
+	if (instruction == "tounix")
+		convert_to_unix(ofs, ifs);
+	else if (instruction == "todos")
+		convert_to_msdos(ofs, ifs);
+	else {
+		cout << "type \"tounix\" or \"todos\" as a third argument";
+	}
+	return 0;
+}
+
+void convert_to_unix(ofstream& ofs, ifstream& ifs) {
+	char c;
+	for (;;) {
+		ifs >> c;
+		if (!ifs.good())
+			break;
+		if (c == '\r')
+			continue;
+		else
+			ofs << c;
+	}
+}
+
+void convert_to_msdos(ofstream& ofs, ifstream& ifs) {
+	char c;
+	for (;;) {
+		ifs >> c;
+		if (!ifs.good())
+			break;
+		if (c == '\n')
+			ofs << "\r\n";
+		else
+			ofs << c;
+	}
+}
