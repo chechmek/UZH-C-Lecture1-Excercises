1478f9d80979016d59ec410a54c0c856e24ca87b
chechmek <romanklut89@gmail.com>
2022-10-14 20:54:49 +0200

    Simple spell checker is done

--- SimpleSpellChecker/SimpleSpellChecker.cpp
+++ SimpleSpellChecker/SimpleSpellChecker.cpp
@@ -4,12 +4,43 @@
 
 using namespace std;
 
+set<string> set_from_file(ifstream& file) {
+	set<string> ::iterator i;
+	set<string> set;
+	string word;
+	while (!file.eof()) {
+		file >> word;
+		set.insert(word);
+	}
+	return set;
+}
+void remove_punctuation(string& str) {
+	str.erase(remove(str.begin(), str.end(), '.'), str.end());
+	str.erase(remove(str.begin(), str.end(), ','), str.end());
+	str.erase(remove(str.begin(), str.end(), '!'), str.end());
+	str.erase(remove(str.begin(), str.end(), '?'), str.end());
+	str.erase(remove(str.begin(), str.end(), '<'), str.end());
+	str.erase(remove(str.begin(), str.end(), '>'), str.end());
+	str.erase(remove(str.begin(), str.end(), '\"'), str.end());
+	str.erase(remove(str.begin(), str.end(), '\''), str.end());
+}
+
 int main(int argc, char* argv[])
 {
-    ifstream dictionaryFile(argv[1]);
-    ifstream textFile(argv[2]);
-    
-    set<string> dictionary;
-    dictionary.
-}
+	ifstream dictionaryFile(argv[1]);
+	ifstream textFile(argv[2]);
+
+	set<string> dictionary = set_from_file(dictionaryFile);
+
+	string word;
+	while (!textFile.eof()) {
+		textFile >> word;
+		remove_punctuation(word);
+		if (dictionary.find(word) == dictionary.end()) { // if not contains
+			cout << word << endl;
+		}
+	}
+};
+
+
 
